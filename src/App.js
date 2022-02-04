ddad33d17f97b8a6ecaf0341600112bf02b653d0
Danilo Cardoso <danilo.cardosolima@gmail.com>
2022-02-04 14:34:45 -0300

    import components and start app layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import ListaDeNotas from './components/ListaDeNotas.jsx'
+
 function App() {
   return (
-    <h1 className='teste'>
-      testando
-    </h1>
+    <section>
+      <form>
+        <input type='text' placeholder="Título" />
+        <textarea placeholder='Escreva sua nota...' />
+        <button>Criar Nota</button>
+      </form>
+      <ListaDeNotas notas={[1,2,3]} />
+    </section>
   );
 }
 
